9185661fa26f2788a14915ba200924bbfa05b035
Алексей <@COSMOS>
2020-10-05 19:09:57 +0300

    Add project files.

---
+++ FloydAlgothitm/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace DeykstraAlgorhitm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.Grid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.OkButton = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Grid
+            // 
+            this.Grid.AllowUserToResizeRows = false;
+            resources.ApplyResources(this.Grid, "Grid");
+            this.Grid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.Grid.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            this.Grid.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.Grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grid.GridColor = System.Drawing.SystemColors.Control;
+            this.Grid.Name = "Grid";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Grid.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // comboBox1
+            // 
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1"),
+            resources.GetString("comboBox1.Items2"),
+            resources.GetString("comboBox1.Items3"),
+            resources.GetString("comboBox1.Items4"),
+            resources.GetString("comboBox1.Items5"),
+            resources.GetString("comboBox1.Items6"),
+            resources.GetString("comboBox1.Items7")});
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // OkButton
+            // 
+            resources.ApplyResources(this.OkButton, "OkButton");
+            this.OkButton.Name = "OkButton";
+            this.OkButton.UseVisualStyleBackColor = true;
+            this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // StartButton
+            // 
+            resources.ApplyResources(this.StartButton, "StartButton");
+            this.StartButton.Name = "StartButton";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // comboBox3
+            // 
+            resources.ApplyResources(this.comboBox3, "comboBox3");
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            resources.GetString("comboBox3.Items"),
+            resources.GetString("comboBox3.Items1"),
+            resources.GetString("comboBox3.Items2"),
+            resources.GetString("comboBox3.Items3"),
+            resources.GetString("comboBox3.Items4"),
+            resources.GetString("comboBox3.Items5"),
+            resources.GetString("comboBox3.Items6"),
+            resources.GetString("comboBox3.Items7")});
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.PathButton_Click);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // comboBox2
+            // 
+            resources.ApplyResources(this.comboBox2, "comboBox2");
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            resources.GetString("comboBox2.Items"),
+            resources.GetString("comboBox2.Items1"),
+            resources.GetString("comboBox2.Items2"),
+            resources.GetString("comboBox2.Items3"),
+            resources.GetString("comboBox2.Items4"),
+            resources.GetString("comboBox2.Items5"),
+            resources.GetString("comboBox2.Items6"),
+            resources.GetString("comboBox2.Items7")});
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // button5
+            // 
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.OkButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Grid);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Grid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ FloydAlgothitm/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DeykstraAlgorhitm
+{
+    public partial class Form1 : Form
+    {
+        const int INF = Int32.MaxValue;
+        int Nodes;//Количество вершин
+        int FirstNode;
+        int [] D; //массив расстояний
+        bool[] V; //Массив посещенных вершин
+        int[,] A; //Матрица расстояний
+        public Form1()
+        {
+            InitializeComponent();
+            Grid.AllowUserToAddRows = false;
+            Grid.EnableHeadersVisualStyles = false;
+            Grid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            Grid.AutoSize = true;
+            Grid.BorderStyle = BorderStyle.None;
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            comboBox2.Items.Clear();
+            comboBox3.Items.Clear();
+            int col = int.Parse(comboBox1.SelectedItem.ToString());
+            for (int i = 1; i <= col; i++)
+            {
+                comboBox3.Items.Add(i);
+                comboBox2.Items.Add(i);
+            }
+            Nodes = int.Parse((string)comboBox1.SelectedItem);
+            D = new int[Nodes];
+            V = new bool[Nodes];
+            A = new int[Nodes, Nodes];
+            for (int i = 0; i < Nodes; i++)
+            {
+                D[i] = INF;
+                V[i] = false;
+                Grid.Columns.Add((i+1).ToString(), (i+1).ToString());
+                Grid.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+                Grid.Columns[i].HeaderCell.Style.BackColor = Color.LightGreen;
+                Grid.Columns[i].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                Grid.Columns[i].HeaderCell.Style.Font = new Font(Grid.Font, FontStyle.Bold);
+            }
+            Grid.Rows.Add(Nodes);
+            for (int i = 0; i < Nodes; i++)
+            {
+                Grid.Rows[i].HeaderCell.Value = string.Format((i + 1).ToString(), "0");
+                Grid.Rows[i].HeaderCell.Style.BackColor = Color.LightGreen;
+                Grid.Rows[i].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                Grid.Rows[i].HeaderCell.Style.Font = new Font(Grid.Font, FontStyle.Bold);
+                for (int j = 0; j < Nodes; j++)
+                    Grid.Rows[i].Cells[j].Value = 0;
+            }
+            Grid.Visible = true;
+        }
+        private void StartButton_Click(object sender, EventArgs e) //Определение минимальных весов вершин
+        {
+            FirstNode = int.Parse(comboBox2.SelectedItem.ToString()) - 1;
+            D[FirstNode] = 0;
+            for (int i = 0; i < Nodes; i++)
+                for (int j = 0; j < Nodes; j++)
+                    A[i, j] = (int)Grid.Rows[i].Cells[j].Value;
+            int TekIndex;
+            while ((TekIndex = Minimum(D, V)) != -1)
+            {
+                for (int i = 0; i < Nodes; i++)
+                    if (A[TekIndex, i] != 0 && V[i] == false) //Есть связь между вершинами и вершина не помечена
+                        D[i] = (D[TekIndex] + A[TekIndex, i] < D[i] ? D[TekIndex] + A[TekIndex, i] : D[i]);
+                V[TekIndex] = true;
+            };
+            textBox2.Clear();
+            for (int i = 0; i < Nodes; i++)
+                textBox2.Text += (i+1).ToString() + " - " + D[i] + ", ";
+        }
+        
+        private void PathButton_Click(object sender, EventArgs e) //Кратчайший путь между вершинами
+        {
+            textBox1.Clear();
+            int source = int.Parse(comboBox3.SelectedItem.ToString()) - 1;
+            int PathLength = 0; //Длина пути
+            textBox1.Text += (source + 1).ToString();
+            while (D[source] > 0)
+            {
+                bool flag = true;
+                for (int i = 0; i < Nodes && flag == true; i++)
+                    if (A[source, i] != 0 && D[source] - A[source, i] == D[i])
+                    {
+                        textBox1.Text = (i + 1).ToString() + " - " + textBox1.Text;
+                        PathLength += D[source];
+                        flag = false;
+                        source = i;
+                    }
+            };
+            textBox1.Text += " path_length = " + PathLength;
+        }
+        private int Minimum(int[] d, bool[] v) //Поиск минимальной вершины по весу из числа непосещенных
+        {
+            int MinIndex = -1;
+            int min = INF;
+            for (int i = 0; i < d.Length; i++)
+                if (v[i] == false && d[i] < min)
+                {
+                    min = d[i];
+                    MinIndex = i;
+                }
+            return MinIndex;
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            Grid.Rows.Clear();
+            Grid.Columns.Clear();
+            comboBox1.Items.Clear();
+            comboBox2.Items.Clear();
+            comboBox3.Items.Clear();
+            textBox1.Clear();
+            textBox2.Clear();
+            comboBox1.Text = "";
+            comboBox2.Text = "";
+            comboBox3.Text = "";
+            OkButton.Enabled = false;
+            StartButton.Enabled = false;
+            button3.Enabled = false;
+            Grid.Visible = false;
+            this.Width = 520;
+            this.Height = 290;
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) => OkButton.Enabled = true;
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e) => StartButton.Enabled = true;
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e) => button3.Enabled = true;
+    }
+}
